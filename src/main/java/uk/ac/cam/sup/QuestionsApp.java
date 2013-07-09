5b5079178ae6a1d1e2f168918ec00a00429047ec
Mariusz Rozycki <mar.rozycki@gmail.com>
2013-07-09 17:13:56 +0100

    Initial commit

---
+++ src/main/java/uk/ac/cam/sup/QuestionsApp.java
@@ -0,0 +1,23 @@
+package uk.ac.cam.sup;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+import uk.ac.cam.sup.controllers.MainController;
+
+import com.googlecode.htmleasy.HtmleasyProviders;
+
+public class QuestionsApp extends Application {
+	public Set<Class<?>> getClasses() {
+		Set<Class<?>> myServices = new HashSet<Class<?>>();
+		
+		myServices.add(MainController.class);
+		
+		// Add Htmleasy Providers
+		myServices.addAll(HtmleasyProviders.getClasses());
+		
+		return myServices;
+	}
+}
---
+++ src/main/java/uk/ac/cam/sup/controllers/MainController.java
@@ -0,0 +1,21 @@
+package uk.ac.cam.sup.controllers;
+
+import java.util.Map;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import com.google.common.collect.ImmutableMap;
+import com.googlecode.htmleasy.ViewWith;
+
+@Path("/")
+public class MainController {
+	
+	@GET
+	@Path("/")
+	@ViewWith("/soy/main.index")
+	public Map indexPage() {
+		return ImmutableMap.of();
+	}
+
+}
