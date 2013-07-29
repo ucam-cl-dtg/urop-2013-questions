dd8414890fe0905ac17ea9e402951fa3c3cb68ff
Mariusz Rozycki <mar.rozycki@gmail.com>
2013-07-29 10:23:54 +0100

    QuestionSet controller divided up into view and edit controllers

--- src/main/java/uk/ac/cam/sup/QuestionsApp.java
+++ src/main/java/uk/ac/cam/sup/QuestionsApp.java
@@ -9,7 +9,8 @@ import uk.ac.cam.sup.controllers.DevelopmentController;
 import uk.ac.cam.sup.controllers.MainController;
 import uk.ac.cam.sup.controllers.MiscController;
 import uk.ac.cam.sup.controllers.QuestionController;
-import uk.ac.cam.sup.controllers.QuestionSetController;
+import uk.ac.cam.sup.controllers.QuestionSetEditController;
+import uk.ac.cam.sup.controllers.QuestionSetViewController;
 import uk.ac.cam.sup.controllers.TagController;
 
 import com.googlecode.htmleasy.HtmleasyProviders;
@@ -19,7 +20,8 @@ public class QuestionsApp extends Application {
 		Set<Class<?>> myServices = new HashSet<Class<?>>();
 		
 		myServices.add(MainController.class);
-		myServices.add(QuestionSetController.class);
+		myServices.add(QuestionSetEditController.class);
+		myServices.add(QuestionSetViewController.class);
 		myServices.add(QuestionController.class);
 		myServices.add(TagController.class);
 		myServices.add(DevelopmentController.class);
