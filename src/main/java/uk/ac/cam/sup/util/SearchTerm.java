834d5e3ed2301b445ffb54a0581affe20c5c5b28
Jocbe <jocbe@atbel.de>
2013-08-02 16:55:33 +0100

    fixes #26 - Fully functional advanced search function now implemented. Some nice-to-have features still missing

--- src/main/java/uk/ac/cam/sup/util/SearchTerm.java
+++ src/main/java/uk/ac/cam/sup/util/SearchTerm.java
@@ -1,9 +1,8 @@
 package uk.ac.cam.sup.util;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Calendar;
-import java.sql.Date;
 import java.util.List;
 
 import uk.ac.cam.sup.exceptions.DateFormatException;
@@ -44,32 +43,6 @@ public class SearchTerm {
 		sanitize();
 	}
 	
-
-	
-	/*private String tagsName = "tags";
-	private String ownersName = "owners";
-	private String starName = "star";
-	private String supervisorName = "supervisor";
-	private String afterName = "after";
-	private String beforeName = "before";
-	private String usageMinName = "usageMin";
-	private String usageMaxName = "usageMax";
-	private String parentIdName = "parentId";
-	private String durMaxName = "durMax";
-	private String durMinName = "durMin";
-	
-	private String tagsTitle = "Tags";
-	private String ownersTitle = "Owners";
-	private String starTitle = "Starred";
-	private String supervisorTitle = "Supervisor only";
-	private String afterTitle = "After date";
-	private String beforeTitle = "Before date";
-	private String usageMinTitle = "Minimum usage";
-	private String usageMaxTitle = "Maximum usage";
-	private String parentIdTitle = "Parent Question";
-	private String durMaxTitle = "Maximum duration";
-	private String durMinTitle = "Minimum duration";
-	*/
 	private enum SanitizationMode {NO_SPACES, ALLOW_SINGLE_SPACES};
 	
 	public void sanitize() throws DateFormatException, InvalidRangeException{
@@ -220,138 +193,7 @@ public class SearchTerm {
 	public void setDurMin(Integer durMin) {
 		this.durMin = durMin;
 	}
-	/*public String getTagsName() {
-		return tagsName;
-	}
-	public void setTagsName(String tagsName) {
-		this.tagsName = tagsName;
-	}
-	public String getOwnersName() {
-		return ownersName;
-	}
-	public void setOwnersName(String ownersName) {
-		this.ownersName = ownersName;
-	}
-	public String getStarName() {
-		return starName;
-	}
-	public void setStarName(String starName) {
-		this.starName = starName;
-	}
-	public String getSupervisorName() {
-		return supervisorName;
-	}
-	public void setSupervisorName(String supervisorName) {
-		this.supervisorName = supervisorName;
-	}
-	public String getAfterName() {
-		return afterName;
-	}
-	public void setAfterName(String afterName) {
-		this.afterName = afterName;
-	}
-	public String getBeforeName() {
-		return beforeName;
-	}
-	public void setBeforeName(String beforeName) {
-		this.beforeName = beforeName;
-	}
-	public String getUsageMinName() {
-		return usageMinName;
-	}
-	public void setUsageMinName(String usageMinName) {
-		this.usageMinName = usageMinName;
-	}
-	public String getUsageMaxName() {
-		return usageMaxName;
-	}
-	public void setUsageMaxName(String usageMaxName) {
-		this.usageMaxName = usageMaxName;
-	}
-	public String getParentIdName() {
-		return parentIdName;
-	}
-	public void setParentIdName(String parentIdName) {
-		this.parentIdName = parentIdName;
-	}
-	public String getDurMaxName() {
-		return durMaxName;
-	}
-	public void setDurMaxName(String durMaxName) {
-		this.durMaxName = durMaxName;
-	}
-	public String getDurMinName() {
-		return durMinName;
-	}
-	public void setDurMinName(String durMinName) {
-		this.durMinName = durMinName;
-	}
-	public String getTagsTitle() {
-		return tagsTitle;
-	}
-	public void setTagsTitle(String tagsTitle) {
-		this.tagsTitle = tagsTitle;
-	}
-	public String getOwnersTitle() {
-		return ownersTitle;
-	}
-	public void setOwnersTitle(String ownersTitle) {
-		this.ownersTitle = ownersTitle;
-	}
-	public String getStarTitle() {
-		return starTitle;
-	}
-	public void setStarTitle(String starTitle) {
-		this.starTitle = starTitle;
-	}
-	public String getSupervisorTitle() {
-		return supervisorTitle;
-	}
-	public void setSupervisorTitle(String supervisorTitle) {
-		this.supervisorTitle = supervisorTitle;
-	}
-	public String getAfterTitle() {
-		return afterTitle;
-	}
-	public void setAfterTitle(String afterTitle) {
-		this.afterTitle = afterTitle;
-	}
-	public String getBeforeTitle() {
-		return beforeTitle;
-	}
-	public void setBeforeTitle(String beforeTitle) {
-		this.beforeTitle = beforeTitle;
-	}
-	public String getUsageMinTitle() {
-		return usageMinTitle;
-	}
-	public void setUsageMinTitle(String usageMinTitle) {
-		this.usageMinTitle = usageMinTitle;
-	}
-	public String getUsageMaxTitle() {
-		return usageMaxTitle;
-	}
-	public void setUsageMaxTitle(String usageMaxTitle) {
-		this.usageMaxTitle = usageMaxTitle;
-	}
-	public String getParentIdTitle() {
-		return parentIdTitle;
-	}
-	public void setParentIdTitle(String parentIdTitle) {
-		this.parentIdTitle = parentIdTitle;
-	}
-	public String getDurMaxTitle() {
-		return durMaxTitle;
-	}
-	public void setDurMaxTitle(String durMaxTitle) {
-		this.durMaxTitle = durMaxTitle;
-	}
-	public String getDurMinTitle() {
-		return durMinTitle;
-	}
-	public void setDurMinTitle(String durMinTitle) {
-		this.durMinTitle = durMinTitle;
-	}*/
+	
 	
 	
 }
--- src/main/webapp/js/search.js
+++ src/main/webapp/js/search.js
@@ -1,57 +1,4 @@
 function searchSetup() {
-	//alert("reload");
-	$("#questions-searchform").on("submit", function(){
-		var $qlist = $("#questionList");
-		$qlist.empty();
-		var newList = document.createElement("div");
-		
-		var searchTerms = "?";
-		var $tokensList = $(".token-input-list");
-		
-		var val; var tmp;
-		
-		$tokensList.children(".token-input-token").each(function(){
-			tmp = $(this).attr("data-type") + "=";
-			val = null;
-			
-			for(var i = 0; i < 3; i++){
-				switch(i){
-					case 0: val = $(this).find(".search-item-input-field").val(); break;
-					case 1: val = $(this).find(".search-item-number").val(); break;
-					case 2: val = $(this).find(".search-item-date").val(); break;
-				}
-				if(val != null){break;}
-			}
-			
-			if(val == null){
-				var $tmp2 = $(this).find(".search-item-checkbox");
-				if ($tmp2.length > 0){
-					($tmp2.is(":checked") ? val = "true" : val = "false");
-				}
-			}
-			
-			if(val != null && val != ""){
-				if(searchTerms != "?"){
-					searchTerms = searchTerms + "&";
-				}
-				searchTerms = searchTerms + tmp + val;
-			}
-			
-		});
-		
-		var permalink = window.location.hash;
-		if(permalink.indexOf("?") > 0) {
-			permalink = permalink.substring(0, window.location.hash.indexOf("?"));
-		}
-		permalink = permalink + searchTerms; 
-		router.navigate(permalink);
-		
-		loadModule($(newList), "q/search" + searchTerms, "questions.search.results");
-		
-		$qlist.append(newList);
-		return false;
-	});
-	
 	$(".add-question-to-set").click(function() {
 		$(this).parents('.list-panel').toggleClass('success');
 	});
@@ -100,6 +47,13 @@ function searchSetup() {
         }
 	});
 	
+	populateSearchField($("#questions-searchform"));
+	
+	$("#questions-searchform").on("submit", function(){
+		search();
+		return false;
+	});
+	search();
 }
 
 function getTokenFormatter(item){
@@ -130,4 +84,188 @@ function getTokenFormatter(item){
 	}
 	
 	return $criteria;
+}
+
+function search(){
+	var $qlist = $("#questionList");
+	$qlist.empty();
+	var newList = document.createElement("div");
+	
+	var searchTerms = "?";
+	var $tokensList = $(".token-input-list");
+	
+	var val; var tmp;
+	
+	$tokensList.children(".token-input-token").each(function(){
+		tmp = $(this).attr("data-type") + "=";
+		val = null;
+		
+		for(var i = 0; i < 3; i++){
+			switch(i){
+				case 0: val = $(this).find(".search-item-input-field").val(); break;
+				case 1: val = $(this).find(".search-item-number").val(); break;
+				case 2: val = $(this).find(".search-item-date").val(); break;
+			}
+			if(val != null){break;}
+		}
+		
+		if(val == null){
+			var $tmp2 = $(this).find(".search-item-checkbox");
+			if ($tmp2.length > 0){
+				($tmp2.is(":checked") ? val = "true" : val = "false");
+			}
+		}
+		
+		if(val != null && val != ""){
+			if(searchTerms != "?"){
+				searchTerms = searchTerms + "&";
+			}
+			searchTerms = searchTerms + tmp + val;
+		}
+		
+	});
+	
+	var permalink = window.location.hash;
+	if(permalink.indexOf("?") > 0) {
+		permalink = permalink.substring(0, window.location.hash.indexOf("?"));
+	}
+	
+	if(searchTerms == "?"){
+		$qlist.append($("<p>Type into search bar at top to find questions...</p>"));
+		return false;
+	}
+	
+	permalink = permalink + searchTerms; 
+	router.navigate(permalink);
+	
+	loadModule($(newList), "q/search" + searchTerms, function(json){
+		if(!json.success){
+			errorNotification(json.error);
+		}
+		return "questions.search.results";
+	});
+	
+	$qlist.append(newList);
+	return false;
+}
+
+function populateSearchField($searchForm){
+	var argStr = window.location.hash;
+	argStr = argStr.substring(argStr.indexOf("?") + 1, argStr.length + 1);
+	var args = argStr.split("&");
+	var arg;
+	var argName;
+	var touched = false;
+	for(var i = 0; i < args.length; i++){
+		arg = args[i].substring(args[i].indexOf("=") + 1, args[i].length + 1);
+		argName = args[i].substring(0, args[i].indexOf("="));
+		if(argName == "tags"){
+			
+			touched = true;
+			$searchForm.find("#txtSearch").tokenInput("add", makeTagSearch());
+			var $tmp = $searchForm.find('.token-input-token[data-type="tags"]').find(".search-item-input-field");
+			var tmp2 = arg.split(",");
+			for(var j = 0; j < tmp2.length; j++){
+				$tmp.tokenInput("add", {value: tmp2[j]});
+			}
+			
+		}else if(argName == "after"){
+			touched = true;
+			$searchForm.find("#txtSearch").tokenInput("add", makeAfterSearch());
+			$searchForm.find('.token-input-token[data-type="after"]').find(".search-item-date").datepicker("setDate", arg);
+			
+		}else if(argName == "before"){
+			touched = true;
+			$searchForm.find("#txtSearch").tokenInput("add", makeBeforeSearch());
+			$searchForm.find('.token-input-token[data-type="before"]').find(".search-item-date").datepicker("setDate", arg);
+			
+		}else if(argName == "durmin"){
+			touched = true;
+			$searchForm.find("#txtSearch").tokenInput("add", makeDurMinSearch());
+			$searchForm.find('.token-input-token[data-type="durmin"]').find(".search-item-number").val(arg);
+			
+		}else if(argName == "durmax"){
+			touched = true;
+			$searchForm.find("#txtSearch").tokenInput("add", makeDurMaxSearch());
+			$searchForm.find('.token-input-token[data-type="durmax"]').find(".search-item-number").val(arg);
+			
+		}else if(argName == "usagemin"){
+			touched = true;
+			$searchForm.find("#txtSearch").tokenInput("add", makeUsageMinSearch());
+			$searchForm.find('.token-input-token[data-type="usagemin"]').find(".search-item-number").val(arg);
+			
+		}else if(argName == "usagemax"){
+			touched = true;
+			$searchForm.find("#txtSearch").tokenInput("add", makeUsageMaxSearch());
+			$searchForm.find('.token-input-token[data-type="usagemax"]').find(".search-item-number").val(arg);
+			
+		}else if(argName == "owners"){
+			touched = true;
+			$searchForm.find("#txtSearch").tokenInput("add", makeOwnersSearch());
+			var $tmp = $searchForm.find('.token-input-token[data-type="owners"]').find(".search-item-input-field");
+			var tmp2 = arg.split(",");
+			for(var j = 0; j < tmp2.length; j++){
+				$tmp.tokenInput("add", {value: tmp2[j]});
+			}
+			
+		}else if(argName == "parents"){
+			touched = true;
+			$searchForm.find("#txtSearch").tokenInput("add", makeParentsSearch());
+			var $tmp = $searchForm.find('.token-input-token[data-type="parents"]').find(".search-item-input-field");
+			var tmp2 = arg.split(",");
+			for(var j = 0; j < tmp2.length; j++){
+				$tmp.tokenInput("add", {value: tmp2[j]});
+			}
+			
+		}else if(argName == "star"){
+			touched = true;
+			$searchForm.find("#txtSearch").tokenInput("add", makeStarSearch());
+			if(arg == "false"){
+				$searchForm.find('.token-input-token[data-type="star"]').find(".search-item-checkbox").attr("checked", false);
+			}
+			
+		}else if(argName == "supervisor"){
+			touched = true;
+			$searchForm.find("#txtSearch").tokenInput("add", makeSupervisorSearch());
+			if(arg == "false"){
+				$searchForm.find('.token-input-token[data-type="supervisor"]').find(".search-item-checkbox").attr("checked", false);
+			}
+		}
+	}
+	if(!touched){
+		$searchForm.find("#txtSearch").tokenInput("add", makeTagSearch());
+	}
+}
+function makeTagSearch(){
+	return {value: "Tags", helpText: "Questions with certain tags", type: "tags", displayType: "text"};
+}
+function makeAfterSearch(){
+	return {value: "After date", helpText: "Only questions after a certain date", type: "after", displayType: "date"};
+}
+function makeBeforeSearch(){
+	return {value: "Before date", helpText: "Only questions before a certain date", type: "before", displayType: "date"};
+}
+function makeDurMinSearch(){
+	return {value: "Min. duration", helpText: "Only questions with a certain min. duration in minutes", type: "durmin", displayType: "number"};
+}
+function makeDurMaxSearch(){
+	return {value: "Max. duration", helpText: "Only questions with a certain max. duration in minutes", type: "durmax", displayType: "number"};
+}
+function makeUsageMinSearch(){
+	return {value: "Min. usage", helpText: "Only questions being used min. _ times", type: "usagemin", displayType: "number"};
+}
+function makeUsageMaxSearch(){
+	return {value: "Max. usage", helpText: "Only questions being used max. _ times", type: "usagemax", displayType: "number"};
+}
+function makeOwnersSearch(){
+	return {value: "Authors", helpText: "Questions by certain authors", type: "owners", displayType: "text"};
+}
+function makeParentsSearch(){
+	return {value: "Parent", helpText: "Only questions with certain parent question IDs", type: "parents", displayType: "text"};
+}
+function makeStarSearch(){
+	return {value: "Starred", helpText: "Only starred/not starred questions", type: "star", displayType: "boolean"};
+}
+function makeSupervisorSearch(){
+	return {value: "Supervisor", helpText: "Only questions by supervisors/students", type: "supervisor", displayType: "boolean"};
 }
