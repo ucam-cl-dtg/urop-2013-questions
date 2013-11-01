426deef690971b63dd23e509b8a0b3f51061ad18
Andrew Rice <acr31@cam.ac.uk>
2013-11-01 12:10:21 +0000

    Updated to recover the particular supervision and also adding suggested tripos questions

--- src/main/python/algorithms2.py
+++ src/main/python/algorithms2.py
@@ -8,6 +8,26 @@ import sys
 import re
 from questions import *
         
+tripos = {
+    '1' : ["2013-p3-q2",
+           "2012-p3-q1",
+           "2010-p3-q2", # a b
+           "2009-p4-q1",
+           "2009-p4-q2"],
+    '2' : [
+        "2013-p3-q1",
+        "2010-p3-q1",
+        "2010-p3-q2", # c d
+        "2008-p4-q2" # = Paper 11 Question 8 
+        ],
+    '3' : [
+        "2012-p3-q2",
+        "2011-p3-q1",
+        "2011-p3-q2",
+        "2007-p4-q9" # = Paper 11 Question 10 
+        ]
+    }
+
 def check(question):
     for field in [question.question,question.solution]:
         if re.search("\\\\defexercise",field):
@@ -18,14 +38,22 @@ def main(filename):
     db = DatabaseOps()
     source = open(filename)
     currentQuestion = None
+    currentSv = None
+    questionSet = None
 
-    questionSet = QuestionSet("Algorithms 2","fms27",["Algorithms2"],db)
 
     for line in source:
         if currentQuestion == None:
-            match = re.match("\\\\defexercise\\{(.*)\\}",line)
+            match = re.match("\\\\defexercise\\{(.*)\\}\\{(.*)\\}",line)
             if match:
-                currentQuestion = Question(match.group(1),"fms27",db)
+                (exercise,sv) = (match.group(1),match.group(2))
+                if sv != currentSv:
+                    questionSet = QuestionSet("Algorithms II - Supervision %s" % sv,"fms27",["Algorithms II"],db)
+                    for triposCode in tripos[sv]:
+                        id = db.lookupQuestionId(triposCode)
+                        db.addToQuestionSet(id,questionSet.questionSetId)
+                    currentSv = sv
+                currentQuestion = Question(exercise,"fms27",db)
                 currentQuestion.question = ""
         elif currentQuestion.solution == None: # accumulating question
             if re.match("\\}\\{",line): # end of question
@@ -36,7 +64,7 @@ def main(filename):
             if re.match("\\}",line): # end of solution
                 check(currentQuestion)
                 currentQuestion.latexToMarkdown()
-                currentQuestion.tags = ["Algorithms2"]
+                currentQuestion.tags = ["Algorithms II"]
                 currentQuestion.save()
                 questionSet.addQuestion(currentQuestion)
                 currentQuestion = None
